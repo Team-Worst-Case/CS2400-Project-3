2ed670c26ee767729302a8214a8c46b07179030a
Roberto Tabares <robctabs08@gmail.com>
2021-10-27 15:51:16 -0700

    completed getHeight method in BinaryNode class

--- BinaryNode.java
+++ BinaryNode.java
@@ -114,16 +114,16 @@ class BinaryNode<T>
     * Computes the height of the subtree rooted at "this" node.
    @return  The height of the subtree rooted at "this" node. */
    public int getHeight_binaryNodeMethod()
-   {  
-	   int leftHeight = 0; 
+   {
+      int leftHeight = 0; 
       int rightHeight = 0;
 
       if (leftChild != null)
          leftHeight = leftChild.getHeight_binaryNodeMethod();
-      else if (rightChild != null)
+      if (rightChild != null)
          rightHeight = rightChild.getHeight_binaryNodeMethod();
 
-      return leftHeight+rightHeight+1;
+      return 1 + leftHeight + rightHeight;
    }
    /** -------------------------------------------------------------------- */
    /** A Recursive Method in the BinaryNode Class  
