f6041793adf01ce3c7575bc1bca05c2a6fdbad23
Roberto Tabares <robctabs08@gmail.com>
2021-10-27 13:45:21 -0700

    started adding changes getHeight_BNM

--- BinaryTree.java
+++ BinaryTree.java
@@ -138,7 +138,7 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
    @return  The height of the "whole" tree. */
    public int getHeight_callBinaryNodeMethod()
    {
-	   return getHeight_binaryNodeMethod();
+	   return root.getHeight_binaryNodeMethod();
    }
 
    /** -------------------------------------------------------------------- */
