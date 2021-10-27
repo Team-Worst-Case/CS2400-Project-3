6780098bf32a2a2a64ae92d2ec3bdc9ffd2c0a8c
Roberto Tabares <robctabs08@gmail.com>
2021-10-27 13:19:35 -0700

    added return statement to getHeight_CBNM method

--- BinaryTree.java
+++ BinaryTree.java
@@ -138,7 +138,7 @@ public class BinaryTree<T> implements BinaryTreeInterface<T>
    @return  The height of the "whole" tree. */
    public int getHeight_callBinaryNodeMethod()
    {
-	   return 0;
+	   return getHeight_binaryNodeMethod();
    }
 
    /** -------------------------------------------------------------------- */
