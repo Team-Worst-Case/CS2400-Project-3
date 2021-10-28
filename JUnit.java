59da82bf2374a79ad09fdb5e51cae4a3c076c069
Christopher <programmerchristopher@gmail.com>
2021-10-27 23:11:19 -0700

    continuing JUnit

--- JUnit.java
+++ JUnit.java
@@ -12,4 +12,30 @@ public class JUnit {
         String traversal = getPOTraversal();
         System.out.println(traversal);
     }
+
+    BinaryTree<String> aTree = new BinaryTree<>();
+    // Leaves
+    BinaryTree<String> dTree = new BinaryTree<>("D");
+    BinaryTree<String> eTree = new BinaryTree<>("E");
+    BinaryTree<String> gTree = new BinaryTree<>("G");
+
+    // Subtrees:
+    BinaryTree<String> fTree = new BinaryTree<>("F", null, gTree);
+    BinaryTree<String> bTree = new BinaryTree<>("B", dTree, eTree);
+    BinaryTree<String> cTree = new BinaryTree<>("C", fTree, null);
+
+    aTree.setTree("A", bTree, cTree);
+    
+    aTree.postorderTraverse();
+    
+    aTree.postorderTraverse_callBinaryNodeMethod();
+    
+    aTree.getHeight();
+    aTree.getHeight_callBinaryNodeMethod();
+    
+    System.out.println();
+    
+    aTree.getNumberOfNodes();
+    aTree.getNumberOfNodes_callBinaryNodeMethod();
+		
 }
