ba4e703633f8ec2bb3febdec377c6d67f4ceb517
Christopher <programmerchristopher@gmail.com>
2021-10-27 22:23:50 -0700

    JUnit startup

--- JUnit.java
+++ JUnit.java
@@ -2,5 +2,14 @@ import org.junit.Test;
 import static org.mockito.Mockito.*;
 
 public class JUnit {
-    
+    protected String getPOTraversal() {
+        String traversal = "";
+
+        return traversal;
+    }
+
+    public void poTraverse() {
+        String traversal = getPOTraversal();
+        System.out.println(traversal);
+    }
 }
