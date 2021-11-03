372a30ceda73a51f6e90db021658ee4125bb5026
setiwon <45044735+setiwon@users.noreply.github.com>
2021-11-02 22:53:46 -0700

    JUnit stuff

--- JUnit.java
+++ JUnit.java
@@ -1,11 +1,12 @@
 import org.junit.Test;
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 public class JUnit {
 
     @Test
     //protected String getPOTraversal() {
-    protected void getPOTraversal() {
+    public void getPOTraversal() {
         //String traversal = "";
         BinaryTree<String> aTree = new BinaryTree<>();
         DriverBT.createTree1(aTree);
@@ -22,7 +23,7 @@ public class JUnit {
     
     @Test
     //protected String getPOTraversal() {
-    protected void getPOTraversal_BinaryNode() {
+    public void getPOTraversal_BinaryNode() {
         //String traversal = "";
         BinaryTree<String> aTree = new BinaryTree<>();
         DriverBT.createTree1(aTree);
@@ -34,25 +35,25 @@ public class JUnit {
     @Test
     public void testGetHeight_binaryNodeMethod()
     {
-	    BinaryTree<String> aTree = new BinaryTree<>();
-	    DriverBT.createTree1(aTree);
-	    assertEquals(4, aTree.getHeight());
+        BinaryTree<String> aTree = new BinaryTree<>();
+        DriverBT.createTree1(aTree);
+        assertEquals(4, aTree.getHeight());
 
-	    BinaryTree<String> bTree = new BinaryTree<>();
-	    DriverBT.createTree2(bTree);
-	    assertEquals(7, bTree.getHeight());
+        BinaryTree<String> bTree = new BinaryTree<>();
+        DriverBT.createTree2(bTree);
+        assertEquals(7, bTree.getHeight());
     }
 
     @Test
-    public void testGetNumberOfNodes_binaryNodeMethod();
+    public void testGetNumberOfNodes_binaryNodeMethod()
     {
-	    BinaryTree<String> aTree = new BinaryTree<>();
-	    DriverBT.createTree1(aTree);
-	    assertEquals(4, aTree.getNumberOfNodes_callBinaryNodeMethod());
+        BinaryTree<String> aTree = new BinaryTree<>();
+        DriverBT.createTree1(aTree);
+        assertEquals(4, aTree.getNumberOfNodes_callBinaryNodeMethod());
 
-	    BinaryTree<String> bTree = new BinaryTree<>();
-	    DriverBT.createTree2(bTree);
-	    assertEquals(7, bTree.getNumberOfNodes_callBinaryNodeMethod());
+        BinaryTree<String> bTree = new BinaryTree<>();
+        DriverBT.createTree2(bTree);
+        assertEquals(7, bTree.getNumberOfNodes_callBinaryNodeMethod());
     }
 
 
